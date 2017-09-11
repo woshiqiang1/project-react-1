829ecb1f4c6d7e19990d1892ffc18eca9e1dc0f5
woshiqiang1 <775106512@qq.com>
2017-09-11 11:44:24 +0800

    add jquery

--- app/index.js
+++ app/index.js
@@ -1,11 +1,9 @@
 import _ from 'lodash';
+import $ from 'jquery';
 function component () {
-  var element = document.createElement('div');
-
-  /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-  return element;
+  var element = $('<div></div>');
+  element.html(_.join(['hello!','webpack'], ' '))
+  return element.get(0);
 }
 
 document.body.appendChild(component());

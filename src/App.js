2b16c0963eb6e00614451dab361aed6f5abba797
woshiqiang1 <775106512@qq.com>
2017-09-12 17:11:12 +0800

    this.submit.bind(this) + add function todo

--- src/App.js
+++ src/App.js
@@ -9,11 +9,8 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            newTodo: 'test',
-            todoList: [
-                {id: 1, title: '第一个待办'},
-                {id: 2, title: '第二个待办'},
-            ]
+            newTodo: '',
+            todoList: []
         }
     }
 
@@ -22,17 +19,17 @@ class App extends Component {
         let todos = this.state.todoList.map((item, index) => {
 
             return ( // 为什么这里要加个括号？这是动手题3 🐸
-                <li>
+                <li key={index}>
                     <TodoItem todo={item}/>
                 </li>
             )
-
         })
+        console.log(todos)
         return (
             <div className="App">
                 <h1>我的待办</h1>
                 <div className="inputWrapper">
-                    <TodoInput content={this.state.newTodo} onSubmit={this.addTodo}/>
+                    <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)}/>
                 </div>
                 <ol>
                     {todos}
@@ -40,6 +37,26 @@ class App extends Component {
             </div>
         )
     }
+
+    addTodo(event) {
+        this.state.todoList.push({
+            id: idMaker(),
+            title: event.target.value,
+            status: null,
+            deleted: false
+        })
+        this.setState({
+            newTodo: '',
+            todoList: this.state.todoList
+        })
+    }
 }
 
 export default App;
+
+let id = 0
+
+function idMaker() {
+    id += 1
+    return id
+}
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,13 +3,13 @@ import React, {Component} from 'react';
 export default class TodoInput extends Component {
     render() {
         return <input type="text" defaultValue={this.props.content}
-                      onKeyPress={this.submit}/>
+                      onKeyPress={this.submit.bind(this)}/>
     }
 
     submit(e) {
         if (e.key === 'Enter') {
             //console.log('用户按回车了');
-            this.props.onSubmit.call()
+            this.props.onSubmit(e)
         }
     }
 }

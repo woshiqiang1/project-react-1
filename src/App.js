772bf00ef727c9833c16e6a7e4968a842562c1d3
woshiqiang1 <775106512@qq.com>
2017-09-13 14:20:52 +0800

    delete test code

--- src/App.js
+++ src/App.js
@@ -7,22 +7,14 @@ import './reset.css'
 import * as localStore from './localStore'
 
 import AV from 'leancloud-storage'
-
-var APP_ID = KHDrL9QXe3FpByNyIfX7iQM5 - gzGzoHsz
-var APP_KEY = 9
-cvT6ffhiDsKj9OO9DxzHUKY
+//初始化leanCloud
+var APP_ID = 'KHDrL9QXe3FpByNyIfX7iQM5-gzGzoHsz';
+var APP_KEY = '9cvT6ffhiDsKj9OO9DxzHUKY';
 AV.init({
     appId: APP_ID,
     appKey: APP_KEY
-})
+});
 
-var TestObject = AV.Object.extend('TestObject')
-var testObject = new TestObject()
-testObject.save({
-    words: 'Hello World!'
-}).then(function (object) {
-    alert('LeanCloud Rocks!')
-})
 
 class App extends Component {
     constructor(props) {

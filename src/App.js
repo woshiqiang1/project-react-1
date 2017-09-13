5e860d717e741d80ba528e56c37926e52162fe78
woshiqiang1 <775106512@qq.com>
2017-09-13 14:24:37 +0800

    新建 leanCould.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import 'normalize.css'
 import './reset.css'
 import * as localStore from './localStore'
 
-import AV from 'leancloud-storage'
-//初始化leanCloud
-var APP_ID = 'KHDrL9QXe3FpByNyIfX7iQM5-gzGzoHsz';
-var APP_KEY = '9cvT6ffhiDsKj9OO9DxzHUKY';
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
-
 
 class App extends Component {
     constructor(props) {
---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'KHDrL9QXe3FpByNyIfX7iQM5-gzGzoHsz';
+var APP_KEY = '9cvT6ffhiDsKj9OO9DxzHUKY';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+export default AV

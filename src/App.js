0e48c44ec73292f43b6536154d0d2627de264585
woshiqiang1 <775106512@qq.com>
2017-09-13 14:07:30 +0800

    test leancloud 功能

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import './reset.css'
 import * as localStore from './localStore'
 
 import AV from 'leancloud-storage'
-var APP_ID = KHDrL9QXe3FpByNyIfX7iQM5-gzGzoHsz
-var APP_KEY = 9cvT6ffhiDsKj9OO9DxzHUKY
+
+var APP_ID = KHDrL9QXe3FpByNyIfX7iQM5 - gzGzoHsz
+var APP_KEY = 9
+cvT6ffhiDsKj9OO9DxzHUKY
 AV.init({
     appId: APP_ID,
     appKey: APP_KEY
+})
+
+var TestObject = AV.Object.extend('TestObject')
+var testObject = new TestObject()
+testObject.save({
+    words: 'Hello World!'
+}).then(function (object) {
+    alert('LeanCloud Rocks!')
 })
 
 class App extends Component {

aa0bfe41c42e22dec38e3fe7fd5c56c3a6a09392
woshiqiang1 <775106512@qq.com>
2017-09-15 16:39:12 +0800

    添加删除数据的功能

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -45,62 +45,69 @@ export const TodoModel = {
     },
     update() {
     },
-    destroy() {
+    destroy(todoId, successFn, errorFn) {
+        // 文档 https://leancloud.cn/docs/leanstorage_guide-js.html#删除对象
+        let todo = AV.Object.createWithoutData('Todo', todoId)
+        todo.destroy().then(function (response) {
+            successFn && successFn.call(null)
+        }, function (error) {
+            errorFn && errorFn.call(null, error)
+        });
     }
-}
 
-export function signUp(email, username, password, successFn, errorFn) {
-    // 新建 AVUser 对象实例
-    var user = new AV.User()
+    export function signUp(email, username, password, successFn, errorFn) {
+        // 新建 AVUser 对象实例
+        var user = new AV.User()
 // 设置用户名
-    user.setUsername(username)
+        user.setUsername(username)
 // 设置密码
-    user.setPassword(password)
+        user.setPassword(password)
 // 设置邮箱
-    user.setEmail(email)
+        user.setEmail(email)
 
-    user.signUp().then(function (loginedUser) {
-        let user = getUserFromAVUser(loginedUser)
-        successFn.call(null, user)
-    }, function (error) {
-        errorFn.call(null, error)
-    })
-    return undefined
-}
-
-export function signIn(username, password, successFn, errorFn) {
-    AV.User.logIn(username, password).then(function (loginedUser) {
-        let user = getUserFromAVUser(loginedUser)
-        successFn.call(null, user)
-    }, function (error) {
-        errorFn.call(null, error)
-    })
-}
-
-export function getCurrentUser() {
-    let user = AV.User.current()
-    if (user) {
-        return getUserFromAVUser(user)
-    } else {
-        return null
+        user.signUp().then(function (loginedUser) {
+            let user = getUserFromAVUser(loginedUser)
+            successFn.call(null, user)
+        }, function (error) {
+            errorFn.call(null, error)
+        })
+        return undefined
     }
-}
 
-export function signOut() {
-    AV.User.logOut()
-    return undefined
-}
+    export function signIn(username, password, successFn, errorFn) {
+        AV.User.logIn(username, password).then(function (loginedUser) {
+            let user = getUserFromAVUser(loginedUser)
+            successFn.call(null, user)
+        }, function (error) {
+            errorFn.call(null, error)
+        })
+    }
 
-export function sendPasswordResetEmail(email, successFn, errorFn) {
-    AV.User.requestPasswordReset(email).then(function (success) {
-        successFn.call()
-    }, function (error) {
-        errorFn.call(null, error)
-    })
-}
+    export function getCurrentUser() {
+        let user = AV.User.current()
+        if (user) {
+            return getUserFromAVUser(user)
+        } else {
+            return null
+        }
+    }
+
+    export function signOut() {
+        AV.User.logOut()
+        return undefined
+    }
+
+    export function sendPasswordResetEmail(email, successFn, errorFn) {
+        AV.User.requestPasswordReset(email).then(function (success) {
+            successFn.call()
+        }, function (error) {
+            errorFn.call(null, error)
+        })
+    }
 
 
-function getUserFromAVUser(AVUser) {
+    function getUserFromAVUser(AVUser)
+{
     return {
         id: AVUser.id,
         ...AVUser.attributes
